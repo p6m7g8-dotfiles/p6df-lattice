14a60af7fe1b539e5752fcdcd601a548fb36bb6d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 11:03:32 -0400

    fix(prompt): replace lattice profile on/off with profile::mod using p6_return_words and add README

--- init.zsh
+++ init.zsh
@@ -7,70 +7,21 @@
 #>
 ######################################################################
 p6df::modules::lattice::deps() {
-  ModuleDeps=()
+  ModuleDeps=(p6m7g8-dotfiles/p6common)
 }
 
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::lattice::prompt::mod()
+# Function: words lattice $LATTICE_API_KEY = p6df::modules::lattice::profile::mod()
 #
 #  Returns:
-#	str - str
+#	words - lattice $LATTICE_API_KEY
 #
-#  Environment:	 P6_DFZ_PROFILE_LATTICE
+#  Environment:	 LATTICE_API_KEY
 #>
 ######################################################################
-p6df::modules::lattice::prompt::mod() {
-  local str=""
-  local profile="$P6_DFZ_PROFILE_LATTICE"
+p6df::modules::lattice::profile::mod() {
 
-  if p6_string_blank_NOT "$profile"; then
-    str="lattice:\t  ${profile}"
-  fi
-
-  p6_return_str "$str"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::lattice::profile::on(profile, code)
-#
-#  Args:
-#	profile -
-#	code - shell code block
-#
-#  Environment:	 P6_DFZ_PROFILE_LATTICE
-#>
-######################################################################
-p6df::modules::lattice::profile::on() {
-  local profile="$1"
-  local code="$2"
-
-  p6_run_code "$code"
-
-  p6_env_export "P6_DFZ_PROFILE_LATTICE" "$profile"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::lattice::profile::off(code)
-#
-#  Args:
-#	code - shell code block previously passed to profile::on
-#
-#  Environment:	 P6_DFZ_PROFILE_LATTICE
-#>
-######################################################################
-p6df::modules::lattice::profile::off() {
-  local code="$1"
-
-  p6_env_unset_from_code "$code"
-  p6_env_export_un P6_DFZ_PROFILE_LATTICE
-
-  p6_return_void
+  p6_return_words 'lattice' '$LATTICE_API_KEY'
 }
